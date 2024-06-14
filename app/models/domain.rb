b0d4a3bfb5611c1a8c75a56d41b4f9bf71194673
Sergey Bondarenko <sergey.bondarenko@flant.com>
2024-06-14 19:14:40 +0300

    refactor: CreationService

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -2,23 +2,17 @@ class Domain < ApplicationRecord
   include AASM
 
   validates :name, presence: true, uniqueness: true
-  after_create :set_aasm_state
 
   aasm do
     state :ok
     state :bad, initial: true
 
     event :success do
-      transitions from: [:ok, :bad], to: :ok
+      transitions from: %i[ok bad], to: :ok
     end
 
     event :failure do
-      transitions from: [:ok, :bad], to: :bad
+      transitions from: %i[ok bad], to: :bad
     end
-  end
-
-  def set_aasm_state
-    result = Domains::CertificateVerificationService.new(self).call
-    result.success? ? success! : failure!
   end
 end
--- app/services/domains/creation_service.rb
+++ app/services/domains/creation_service.rb
@@ -1,8 +1,21 @@
 class Domains::CreationService < Service
   def call(params)
-    domain = Domain.create(params)
-    return form_error(domain) unless domain.save
+    domain = Domain.build(params)
+
+    ApplicationRecord.transaction do
+      return form_error(domain) unless domain.save
+
+      init_state(domain)
+    end
 
     success(domain)
+  end
+
+  private
+
+  def init_state(domain)
+    result = CertificateVerificationService.new(domain).call
+
+    result.success? ? domain.success! : domain.failure!
   end
 end
